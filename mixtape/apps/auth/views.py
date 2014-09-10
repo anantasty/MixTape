191f84d225e81111f913b58efa6bf91eddfaaafd
Anant <anant.asty@gmail.com>
2014-09-09 20:36:50 -0600

    code with horrible debug statemewnts

--- mixtape/apps/auth/views.py
+++ mixtape/apps/auth/views.py
@@ -8,15 +8,18 @@ class SoundCloudLogin(View):
     form_class = SoundCloudLoginForm
 
     def get(self, request):
-        form = SoundCloudLoginForm(request)
-        code = form.fields['code']
-        print code
-        return http.JsonResponse({'code': code})
+        print "in req session_id = {}".format(request.session.session_key)
+        form = SoundCloudLoginForm(request.GET)
+        if form.is_valid():
+            code = form.cleaned_data['code']
+            return http.JsonResponse({'code': code})
+        return http.JsonResponse({'code': 'FAILED'})
 
 
 class SoundCloudGetUrl(View):
 
     def get(self, requeest):
+        print "out_req session_id = {}".format(request.session.session_key)
         return http.HttpResponseRedirect(soundcloud_utils.get_login_url())
 
 
